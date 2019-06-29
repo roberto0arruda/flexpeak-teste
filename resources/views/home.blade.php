ba2f0a05fff76187736ccdc0399b6c5b56a3e06a
Roberto Arruda <roberto0arruda@hotmail.com>
2019-06-29 11:26:58 -0400

    implemented adminlte template front-end with lang/pt-br

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('adminlte::page')
+
+@section('title', 'Home')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>You are logged in!</p>
+    <p>Você está logado!</p>
+    <p>Use as opções ao lado para continuar!</p>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
+Route::get('/site', function () {
     return view('welcome');
 });
+
+Route::redirect('/', '/site', 301);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
