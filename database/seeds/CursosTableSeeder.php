94050dcacccdd0de2a9b7a03212b158d5315f489
Roberto Arruda <roberto0arruda@hotmail.com>
2019-08-08 16:55:26 -0400

    implemented table seeder Cursos

---
+++ database/seeds/CursosTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CursosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\Models\Curso::class, 10)->create();
+    }
+}
